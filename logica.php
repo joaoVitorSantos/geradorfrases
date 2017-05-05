f8bd9d25a15c04fe662b33a44dfd017fbe474548
joaoVitorSantos <joaovitorjec@gmail.com>
2017-05-05 09:28:04 -0300

    Upload dos arquivos iniciais da aplicação

---
+++ logica.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$tratamentos = ['Lendário', 'Virtuoso', 'João O Grande'];
+$qualidades = ['O NEGRO DO SÉCULO', 'O Sábio', 'O Pensador'];
+$lugares = ['Das terras médias', 'De Machu Pichu', 'De Joinvas'];
+$nome = "João Vitor";
+
+$tratamento = $tratamentos[rand(0,2)];
+$qualidade = $qualidades[rand(0,2)];
+$lugar = $lugares[rand(0,2)];
